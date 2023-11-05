c686c94727e7a6f41c72819a28d5d427e4c5838f
Mahrunisa01 <149329215+Mahrunisa01@users.noreply.github.com>
2023-11-05 14:22:54 -0800

    Add files via upload

---
+++ booking-flower.js
@@ -0,0 +1,42 @@
+
+import rose from "../images/rose.jpg"
+import daisy from "../images/daisy.jpg"
+import lavender from "../images/lavenders.jpg"
+import lotus from "../images/lotus.jpg"
+import sunflower from "../images/sunflower.jpg"
+
+const bookingFlower = [
+  {
+    id: "01",
+    FlowerName: "Rose",
+    Price: 89,
+    imgUrl: rose,
+  },
+  {
+    id: "02",
+    FlowerName: "Daisy",
+    Price: 89,
+    imgUrl: daisy,
+  },
+  {
+    id: "03",
+    FlowerName: "Lavender",
+    Price: 89,
+    imgUrl: lavender,
+  },
+  {
+    id: "04",
+    FlowerName: "Lotus",
+    Price: 89,
+    imgUrl: lotus,
+  },
+  {
+    id: "05",
+    FlowerName: "Sunflower",
+    Price: 89,
+    imgUrl: sunflower,
+  },
+  
+];
+
+export default bookingFlower;
---
+++ recommendFlowers.js
@@ -0,0 +1,36 @@
+import rose from "../images/rose.jpg"
+import lavender from "../images/lavenders.jpg"
+import lotus from "../images/lotus.jpg"
+import daisy from "../images/daisy.jpg"
+
+const recommendFlowers = [
+
+    {
+        id: "01",
+        FlowerName: "lotus",
+        Price: 89,
+        imgUrl: lotus,
+        rentPrice: 32,
+        retweet: "13",
+      },
+
+      {
+        id: "02",
+        FlowerName: "daisy",
+        Price: 89,
+        imgUrl: daisy,
+        rentPrice: 32,
+        retweet: "12",
+      },
+
+      {
+        id: "03",
+        FlowerName: "Lavender",
+        Price: 89,
+        imgUrl: lavender,
+        rentPrice: 32,
+        retweet: "14",
+      },
+];
+
+export default recommendFlowers;
